2e74a51aa86ac3d63f79d180aa292b9863418cd0
Karandeep Johar <karandeepjohar@gmail.com>
2016-04-05 00:57:41 -0400

    init fix

--- src/init.py
+++ src/init.py
@@ -7,7 +7,7 @@ import os
 dir = os.path.dirname(__file__)
 filename = os.path.join(dir, '../stanford-corenlp-python/stanford-corenlp-full-2014-08-27/*')
 configFileLoc = os.path.join(dir, 'config.ini')
-# proc1 = CoreNLP(configfile=configFileLoc, corenlp_jars=[filename])
+proc1 = CoreNLP(configfile=configFileLoc, corenlp_jars=[filename])
 proc2 = CoreNLP(configfile=os.path.join(dir, 'simple.ini'), corenlp_jars=[filename])
 proc3 = CoreNLP(configfile=os.path.join(dir, 'mid.ini'), corenlp_jars=[filename])
 puncTags = ['.',',','IN','#','$','CC','SYM','-LRB-','-RRB-',"''",'``',"'","`",'"',':',';','[',']','{','}','-','_','!','?','~','&','*']
