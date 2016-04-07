b62208b50c13863d7efa88ffe25b739573357011
Dhruv Anand <danand@andrew.cmu.edu>
2016-04-07 16:43:02 -0400

    removal of small sentences from interesting text

--- src/tfidf.py
+++ src/tfidf.py
@@ -53,9 +53,10 @@ class TF_IDF(object):
         newQBOW = self.dictionary.doc2bow(proc.parse_doc(question)['sentences'][0]['lemmas'])
         answerSentences = self.index[self.tfidf[newQBOW]]
         answerSentences = sorted(answerSentences, key=lambda tup: tup[1], reverse=True)
-        answerSentences = [(x[1],\
-            self.tokenized['sentences'][x[0]]
-            ) for x in answerSentences]
+        for x in answerSentences:
+            if (len(set(self.tokenized['sentences'][x[0]]['lemmas'])-set(puncTags))<3):
+                answerSentences.remove(x)
+        answerSentences = [(x[1], self.tokenized['sentences'][x[0]]) for x in answerSentences]
         return answerSentences
     
     def getAnswer(self,question,answerSentences, qpobj):
@@ -85,61 +86,3 @@ class TF_IDF(object):
             answersTFIDF.append(self.tfidf[answerBOW])
         answerIndex = similarities.SparseMatrixSimilarity(answersTFIDF, num_features=len(self.dictionary),num_best=len(answersTFIDF))
         return answerIndex[groundTruthTFIDF]
-
-# # Tokenization
-# tokenized = proc.parse_doc(open('wiki.txt').read())
-# questionList = [proc.parse_doc(q)['sentences'][0]['lemmas']
-#                 for q in open('questions.txt').read().split('\n')]
-
-# # Extracting tokenized sentences
-# sentenceList = [s['lemmas'] for s in tokenized['sentences']]
-# numSentences = len(sentenceList)
-
-# # Generating dictionary
-# dictionary = corpora.Dictionary(sentenceList + questionList)
-# # dictionary.save('/tmp/corpDict.dict')
-
-# # Converting corpus and questions to vector representation
-# corpusVectors = [dictionary.doc2bow(c) for c in sentenceList]
-# qBOW = [dictionary.doc2bow(c) for c in questionList]
-
-# # corpora.MmCorpus.serialize('/tmp/corpVect.mm', corpusVectors)
-# # dictionary = corpora.Dictionary.load('/tmp/corpDict.dict')
-# # corpus = corpora.MmCorpus('/tmp/corpVect.mm')
-
-# # Initializing TF-IDF model
-# tfidf = models.TfidfModel(corpusVectors)
-
-# # Extracting TF-IDF of documents
-# corpusTfidf = tfidf[corpusVectors]
-# index = similarities.SparseMatrixSimilarity(corpusTfidf, num_features=len(dictionary), num_best=5)
-
-# # index.save('/tmp/deerwester.index')
-# # index = similarities.MatrixSimilarity.load('/tmp/deerwester.index')
-# sims = []
-# # Similarity computation
-# for q in qBOW:
-#     sims.append(index[tfidf[q]])
-# # pp.pprint(sims)
-# sortedAnswers = []
-# for answerSentences in sims:
-#     answerSentences = sorted(answerSentences, key=lambda tup: tup[1], reverse=True)
-#     sortedAnswers.append(answerSentences)
-# # print(sortedAnswers)
-# # sortedSims.filter(lambda tup:tup[0]<numSentences)
-# for s in sortedAnswers:
-#     for x in s:
-#         print 'Potential sentence (score:%.2f' % (x[1],), '):',
-#         print ' '.join(tokenized['sentences'][x[0]]['tokens'])
-#     print '\n'
-
-# # Potential Answer Generation
-# for i in range(len(sortedAnswers)):
-#     s = sortedAnswers[i]
-#     x = s[0]
-#     print 'Potential Answer:',
-#     for j in range(len(tokenized['sentences'][x[0]]['lemmas'])):
-#         w = tokenized['sentences'][x[0]]['lemmas'][j]
-#         if w.lower() not in map(lambda x: x.lower(), questionList[i]):
-#             print tokenized['sentences'][x[0]]['tokens'][j],
-#     print '\n'
