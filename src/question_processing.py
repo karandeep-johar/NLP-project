24c28bf32c30374f9f185c09e6375b0260be8f4e
Dhruv Anand <danand@andrew.cmu.edu>
2016-04-11 22:50:18 -0400

    Changed wh word capturing technique

--- src/question_processing.py
+++ src/question_processing.py
@@ -5,12 +5,12 @@ class Question_parser:
 
     # def __init__(self, question):
     #     self.valid = False
-    #     self.qtype = "NA"
-    #     self.answer_type = "NA"
-    #     self.difficulty = "NA"
+    #     self.qtype = 'NA'
+    #     self.answer_type = 'NA'
+    #     self.difficulty = 'NA'
     #     self.question = question
     #     self.parse()
-    def __init__(self, question, valid=False, qtype= "NA", answer_type ="NA", difficulty = "NA", answer = "NA", parseFlag = True, article_title = "NA", dataset = "NA"):
+    def __init__(self, question, valid=False, qtype= 'NA', answer_type ='NA', difficulty = 'NA', answer = 'NA', parseFlag = True, article_title = 'NA', dataset = 'NA'):
         self.valid = valid
         self.qtype = qtype
         self.answer_type = answer_type
@@ -27,45 +27,58 @@ class Question_parser:
     def __cmp__(self, other): 
             return self.__dict__ == other.__dict__
     def __str__(self):
-        return '\n' + self.question + "\n" +\
-            "Validity= " + str(self.valid) + '\n' +\
-            "Question Type = " + self.qtype + '\n' +\
-            "Answer Type = " + str(self.answer_type) + '\n' +\
-            "Answer = " + self.answer + '\n' +\
-            "Difficulty = " + self.difficulty + '\n' +\
-            "Article_Title = " + self.article_title + '\n' +\
-            "Dataset = " + self.dataset + '\n'
+        return '\n' + self.question + '\n' +\
+            'Validity= ' + str(self.valid) + '\n' +\
+            'Question Type = ' + self.qtype + '\n' +\
+            'Answer Type = ' + str(self.answer_type) + '\n' +\
+            'Answer = ' + self.answer + '\n' +\
+            'Difficulty = ' + self.difficulty + '\n' +\
+            'Article_Title = ' + self.article_title + '\n' +\
+            'Dataset = ' + self.dataset + '\n'
 
     def parse(self):
+        AUX_QWORDS = set(['be', 'do','does','have','can','could', 'will', 'would'])
+        OTHER_QWORDS = set(['who','whom','when','where','how'])
         proc = init.proc1
         parsed = proc.parse_doc(self.question)
         if parsed['sentences'][0]['tokens'][-1] == '?':
             self.valid = True
             # pprint(parsed)
-            first_word = parsed['sentences'][0]['lemmas'][0]
-            if first_word.lower() in ['be', 'do','does',"have","can","could", "will", "would"]:
+            wh_word=''
+            for i in len(parsed['sentences'][0]['lemmas']):
+                if parsed['sentences'][0]['lemmas'][i] in (AUX_QWORDS|OTHER_QWORDS):
+                    wh_word = parsed['sentences'][0]['lemmas'][i]
+                    break
+            if wh_word=='':
+                self.valid = False
+                return
+            wh_wordL = wh_word.lower()
+            if wh_wordL in AUX_QWORDS:
                 self.qtype = 'BOOLEAN'
                 self.difficulty = 'easy'
-                self.answer_type = set(["BOOLEAN"])
-            else:
-                self.qtype = "Factoid"
+                self.answer_type = set(['BOOLEAN'])
+            elif wh_wordL in OTHER_QWORDS:
+                self.qtype = 'Factoid'
                 self.difficulty = 'medium'
-                wh_word = parsed[u'sentences'][0][u'tokens'][0]
-                if wh_word.lower() == 'who' or wh_word.lower() == "whom":
-                    self.answer_type = set(["PERSON","ORGANIZATION"])
-                elif wh_word.lower() == 'when':
-                    self.answer_type = set(["TIME","DATE"])
-                elif wh_word.lower() == 'where':
-                    self.answer_type = set(["LOCATION"])
-                elif wh_word.lower() == 'how':
-                    self.answer_type = set(["NUMBER","MONEY", "TIME","DATE", "PERCENT"])
-                else:
-                    self.difficulty = 'Unknown'
-                    self.difficulty = 'hard'
-                    if wh_word.lower() == 'what':
-                        self.answer_type = set(["UNKNOWN"])
+                if wh_wordL == 'who' or wh_wordL == 'whom':
+                    self.answer_type = set(['PERSON','ORGANIZATION'])
+                elif wh_wordL == 'when':
+                    self.answer_type = set(['TIME','DATE'])
+                elif wh_wordL == 'where':
+                    self.answer_type = set(['LOCATION'])
+                elif wh_wordL == 'how':
+                    if parsed['sentences'][0]['tokens'][i+1].lower() not in set(['long','much','many','far']):
+                        self.answer_type = set(['UNKNOWN'])
+                        self.difficulty = 'hard'
                     else:
-                        self.answer_type = set(["UNKNOWN"])
+                        self.answer_type = set(['NUMBER','MONEY', 'TIME','DATE', 'PERCENT'])
+                else:
+                    self.difficulty = 'hard'
+                    if wh_wordL == 'what':
+                        self.answer_type = set(['UNKNOWN'])
+                    else:
+                        self.answer_type = set(['UNKNOWN'])
+                
             # Why => reason
             # what => all
             # which =>all
@@ -84,37 +97,37 @@ class Question_parser:
 
 
 if __name__ == '__main__':
-    question1 = "Does Portuguese contain words from the Arabic language?"
+    question1 = 'Does Portuguese contain words from the Arabic language?'
     q1_parse = Question_parser(question1)
     print q1_parse
-    # print "hello, it's me!"
-    question2 = "Did United defeat Chelsea?"
+    # print 'hello, it's me!'
+    question2 = 'Did United defeat Chelsea?'
     q2_parse = Question_parser(question2)
     print q2_parse
-    question3 = "Have you reached home?"
+    question3 = 'Have you reached home?'
     q3_parse = Question_parser(question3)
-    question3 = "Is it raining outside?"
+    question3 = 'Is it raining outside?'
     q3_parse = Question_parser(question3)
     print q3_parse
-    question4 = "Who killed John Lennon?"
+    question4 = 'Who killed John Lennon?'
     q4_parse = Question_parser(question4)
     print q4_parse
-    question5 = "Where are they giving free food?"
+    question5 = 'Where are they giving free food?'
     q5_parse = Question_parser(question5)
     print q5_parse
-    question6 = "When is the concert?"
+    question6 = 'When is the concert?'
     q6_parse = Question_parser(question6)
     print q6_parse
-    question7 = "Would Karan become the president of the United States?"
+    question7 = 'Would Karan become the president of the United States?'
     q7_parse = Question_parser(question7)
     print q7_parse
-    question8 = "In what book can I find the story of Alladin?"
+    question8 = 'In what book can I find the story of Alladin?'
     q8_parse = Question_parser(question8)
     print q8_parse
-    question8 = "Can a flute be played with several different air sources?"
+    question8 = 'Can a flute be played with several different air sources?'
     q8_parse = Question_parser(question8)
     print q8_parse
-    question8 = "Could Malay have originated from Sumatra island?"
+    question8 = 'Could Malay have originated from Sumatra island?'
     q8_parse = Question_parser(question8)
     print q8_parse
     
@@ -125,17 +138,17 @@ if __name__ == '__main__':
 # pp.pprint(parsed)
 # print parsed)
 # print parsed.keys()
-#	print "Yes, it is question"
+#	print 'Yes, it is question'
 # Is it a yes/no question:
 
-#		print "Yes-No"
+#		print 'Yes-No'
 # Invoke TFIDF and if score is high- Easy
 # else hard
 #	else:
-#		print "Factoid"
+#		print 'Factoid'
 # Find type of factoid # CHeck wh word
 
 #
 # else:
-#	print "No, not a question"
+#	print 'No, not a question'
 # pp.pprint(parsed)
